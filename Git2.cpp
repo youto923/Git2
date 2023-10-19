c80e404393d47ad771507cdeec06d6fd88e484ef
優斗 竹田 <k023g0018@g.neec.ac.jp>
2023-10-19 11:39:42 +0900

    日付

--- Git2.cpp
+++ Git2.cpp
@@ -6,6 +6,10 @@
 int main()
 {
     std::cout << "Hello World!\n";
+    std::cout << "2023/10/11\n";
+    std::cout << "LC1D_15_タケダ_ユウト\n";
+
+
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
